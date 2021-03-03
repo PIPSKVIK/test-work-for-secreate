3f36ed6d1b0e1d74e8f817fb399abda453190ea5
Alexander <milovanovska@yandex.ru>
2021-03-03 22:34:04 +0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,162 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="container">
+      <form @submit.prevent="addTodo">
+        <div class="container__create-todo create-todo wrapper">
+          <div class="create-todo__header">
+            <h3 class="create-todo__title">TODO list: {{ todos.length }}</h3>
+            <md-button
+              type="submit"
+              class="md-raised md-accent"
+              :disabled="buttonCreateDisabled"
+            >
+              Add
+            </md-button>
+          </div>
+          <md-field class="create-todo__input-title">
+            <label>Заголовок - ({{ title.length }})</label>
+            <md-input type="text" v-model="title" require />
+            <span class="md-error">Invalid first name</span>
+          </md-field>
+          <md-field class="create-todo__input-description">
+            <label>Описание - ({{ this.description.length }})</label>
+            <md-textarea v-model="description" />
+          </md-field>
+        </div>
+      </form>
+      <div class="container__todo-list todo-list wrapper">
+        <ul class="todo-list__item" v-if="todos.length">
+          <todo-element
+            v-for="(todo, i) in todos"
+            :key="todo.id"
+            :index="i"
+            :todo="todo"
+            @delete-todo="deleteTodo"
+            @save-todo="saveTodo"
+          />
+        </ul>
+        <span class="todo-list__empty" v-else>Пусто</span>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoElement from "@/components/TodoElement";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: { TodoElement },
+  data() {
+    return {
+      todos: [],
+      title: "",
+      description: "",
+      invalid: true,
+      buttonCreateDisabled: true
+    };
+  },
+  watch: {
+    description() {
+      if (this.description.length >= 300) {
+        this.buttonCreateDisabled = true;
+      }
+    },
+    title() {
+      if (this.title.length >= 3 && this.title.length <= 64) {
+        this.buttonCreateDisabled = false;
+      } else {
+        this.buttonCreateDisabled = true;
+      }
+    },
+  },
+  created() {
+    if (localStorage.getItem("todos")) {
+      this.todos = JSON.parse(localStorage.getItem("todos"));
+    }
+  },
+  methods: {
+    addTodo() {
+      const createId = Date.now();
+
+      this.todos.push({
+        id: createId,
+        title: this.title,
+        date: new Date().toLocaleString(navigator.language, {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "numeric"
+        }),
+        description: this.description
+      });
+      this.title = "";
+      this.description = "";
+      this.saveLocalStorageTodos();
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id);
+      this.saveLocalStorageTodos();
+    },
+    saveTodo() {
+      this.saveLocalStorageTodos();
+    },
+    saveLocalStorageTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+      this.totos = JSON.parse(localStorage.getItem("todos"));
+    }
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.container {
+  background: rgba(29, 29, 29, 0.2);
+  width: 100vw;
+  height: 100vh;
+  padding-top: 40px;
+}
+
+.create-todo {
+  &__header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  &__title {
+    font-size: 30px;
+    color: #0040ea;
+    margin: 0;
+  }
+
+  &__input-title {
+    margin: 0;
+    height: 50px;
+  }
+}
+.todo-list {
+  height: 410px;
+  overflow: auto;
+  background-color: #dddddd;
+  box-shadow: 20px 15px 10px 5px rgba(0, 0, 0, 0.5);
+  position: relative;
+  &__item {
+    padding: 10px;
+  }
+  &__empty {
+    position: absolute;
+    font-size: 30px;
+    align-self: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+
+.wrapper {
+  width: 500px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/TodoElement.vue
@@ -0,0 +1,123 @@
+<template>
+  <li class="todo-item">
+    <span class="todo-item__index"># {{ index + 1 }}</span>
+    <div class="todo-item__content content">
+      <md-field v-if="!isTextEditDisabled" class="content__title">
+        <md-input v-model="localTodo.title" :disabled="isTextEditDisabled" />
+      </md-field>
+      <span class="content__title-view" v-else>{{ localTodo.title }}</span>
+      <div class="todo-item__control control">
+        <div class="control__all-button">
+          <md-button
+            @click="$emit('delete-todo', localTodo.id)"
+            class="md-icon-button control__del"
+          >
+            <md-icon :md-src="require('./icon/icon-delete.svg')" />
+          </md-button>
+          <md-button
+            @click="handleSave"
+            class="md-icon-button control__save"
+            :disabled="isDisabled"
+            :class="{ 'button-disabled': isDisabled }"
+          >
+            <md-icon :md-src="require('./icon/icon-save.svg')" />
+          </md-button>
+          <md-button
+            class="md-icon-button control__edit"
+            @click="isTextEditDisabled = !isTextEditDisabled"
+          >
+            <md-icon :md-src="require('./icon/icon-edit.svg')" />
+          </md-button>
+        </div>
+        <span class="control__date">{{ localTodo.date }}</span>
+      </div>
+    </div>
+    <md-field class="todo-item description" v-if="!isTextEditDisabled">
+      <md-textarea
+        class="description__element"
+        v-model="localTodo.description"
+        :disabled="isTextEditDisabled"
+      />
+    </md-field>
+    <div class="description__element-preview" v-else>{{ localTodo.description }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoElement",
+  components: {},
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: Number
+  },
+  data() {
+    return {
+      localTodo: this.todo,
+      isTextEditDisabled: true,
+      isDisabled: true
+    };
+  },
+  watch: {
+    localTodo: {
+      deep: true,
+      handler() {
+        this.isDisabled = false;
+      }
+    }
+  },
+  methods: {
+    handleSave() {
+      this.isDisabled = true;
+      this.isTextEditDisabled = true;
+      this.$emit("save-todo");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  list-style: none;
+  border-bottom: 1px solid black;
+  padding: 5px;
+  &__index {
+    font-size: 20px;
+  }
+}
+
+.content {
+  display: flex;
+  justify-content: space-between;
+  &__title-view {
+    color: cornflowerblue;
+    font-size: 18px;
+    text-transform: uppercase;
+  }
+}
+
+.control {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &__date {
+    font-size: 10px;
+  }
+  &__all-button {
+    display: flex;
+  }
+}
+
+.description {
+  &__element-preview {
+    overflow: hidden;
+  }
+}
+
+.button-disabled {
+  background-color: #ff6767;
+}
+</style>
